4b85f810b1eee7914ec520e1677e0182cac39fb7
ravi-pinapaka <ravikumar.psvk@gmail.com>
2021-11-24 10:33:15 +0000

    Unit testing updated

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    version = '1.0.1';
+    version = '1.1.0';
     team = 'Team Phoenix';
 }
 module.exports = AppConfig;
